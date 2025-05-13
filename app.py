8c4845196b99d599cbef8889582311d1d7dbcc73
a-miaw <nmtydatsu@gmail.com>
2025-05-14 01:02:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-import os time
+import os, time
 from tensorflow.keras.preprocessing.image 
 import ImageDataGenerator
 from PIL import Image
